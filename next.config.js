c569dc42babce215845b6ed1f96513c7037c6584
北䑓如法 <algebraicallyClosedField@gmail.com>
2019-06-24 23:45:58 +0900

    Set output directory

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
 const withSass = require('@zeit/next-sass');
 const withCSS = require('@zeit/next-css')
-module.exports = withCSS(withSass())
+module.exports = withCSS(withSass({
+  distDir: 'build_dir',
+  exportPathMap: async function(
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      // '/about.md': { page: '/about' },
+      // '/team.md': { page: '/team' },
+      // '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } }
+    }
+  },
+}))
