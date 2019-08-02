834c004845df3ff9067835c526c44df36dde4e6b
北䑓如法 <algebraicallyClosedField@gmail.com>
2019-08-03 03:48:36 +0900

    next export again

---
+++ docs/_next/static/development/pages/index.js
@@ -0,0 +1,116 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static/development/pages/index.js"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fnyoho%2Fprog%2FStaticSiteGenerators%2Fnext-pyconhiro-test%2Fpages%2Findex.js!./":
+/*!****************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fnyoho%2Fprog%2FStaticSiteGenerators%2Fnext-pyconhiro-test%2Fpages%2Findex.js ***!
+  \****************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/", function() {
+      var page = __webpack_require__(/*! ./pages/index.js */ "./pages/index.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/index.js */ "./pages/index.js", function() {
+          if(!next.router.components["/"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/index.js */ "./pages/index.js")
+          next.router.update("/", updatedPage.default || updatedPage)
+        })
+      }
+      return { page: page.default || page }
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_1aef2d0bbc0d334d831c ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_1aef2d0bbc0d334d831c */ "dll-reference dll_1aef2d0bbc0d334d831c"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/nyoho/prog/StaticSiteGenerators/next-pyconhiro-test/pages/index.js";
+
+
+var isSMILAvailable = function isSMILAvailable() {
+  if (typeof window === 'undefined') {
+    return true;
+  } else {
+    return window.navigator.userAgent.match(/(MSIE|Trident|Edge)\//) === null;
+  }
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12
+    },
+    __self: this
+  }, "Hello, Next.js world!", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("svg", {
+    id: "some-graphics",
+    "data-name": "some-graphics",
+    xmlns: "http://www.w3.org/2000/svg",
+    viewBox: "0 0 300 300",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 14
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("text", {
+    x: isSMILAvailable() ? "0" : "100",
+    y: "20",
+    fontFamily: "sans-serif",
+    fontSize: "16",
+    fill: "#f4267c",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 15
+    },
+    __self: this
+  }, "SVG Text", isSMILAvailable() ? "Ok" : "No")));
+});
+
+/***/ }),
+
+/***/ 2:
+/*!********************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2F&absolutePagePath=%2FUsers%2Fnyoho%2Fprog%2FStaticSiteGenerators%2Fnext-pyconhiro-test%2Fpages%2Findex.js ***!
+  \********************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2F&absolutePagePath=%2FUsers%2Fnyoho%2Fprog%2FStaticSiteGenerators%2Fnext-pyconhiro-test%2Fpages%2Findex.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fnyoho%2Fprog%2FStaticSiteGenerators%2Fnext-pyconhiro-test%2Fpages%2Findex.js!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_1aef2d0bbc0d334d831c":
+/*!*******************************************!*\
+  !*** external "dll_1aef2d0bbc0d334d831c" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_1aef2d0bbc0d334d831c;
+
+/***/ })
+
+},[[2,"static/runtime/webpack.js"]]]);
+//# sourceMappingURL=index.js.map
---
+++ docs/_next/static/webpack/static/development/pages/index.js.71b6685ce122669e8b1e.hot-update.js
@@ -0,0 +1,59 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/nyoho/prog/StaticSiteGenerators/next-pyconhiro-test/pages/index.js";
+
+
+var isSMILAvailable = function isSMILAvailable() {
+  if (typeof window === 'undefined') {
+    return true;
+  } else {
+    return window.navigator.userAgent.match(/(MSIE|Trident|Edge)\//) === null;
+  }
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12
+    },
+    __self: this
+  }, "Hello, Next.js world!", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("svg", {
+    id: "some-graphics",
+    "data-name": "some-graphics",
+    xmlns: "http://www.w3.org/2000/svg",
+    viewBox: "0 0 300 300",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 14
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("text", {
+    x: isSMILAvailable() ? "0" : "100",
+    y: "20",
+    fontFamily: "sans-serif",
+    fontSize: "16",
+    fill: "#f4267c",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 15
+    },
+    __self: this
+  }, "SVG Text", isSMILAvailable() ? "1Ok" : "No")));
+});
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.71b6685ce122669e8b1e.hot-update.js.map
