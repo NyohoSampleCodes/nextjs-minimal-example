66e00c37b3343e6f3c3b085b59ba5e67132028ea
北䑓如法 <algebraicallyClosedField@gmail.com>
2019-08-03 03:09:06 +0900

    Add browser detection code

--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,20 @@
 import React from 'react'
-export default () => <div>Hello, Next.js world!</div>
+
+const isSMILAvailable = () => {
+  if (typeof window === 'undefined') {
+    return true;
+  } else {
+    return window.navigator.userAgent.match(/(MSIE|Trident|Edge)\//) === null;
+  }
+}
+
+export default () =>
+  <div>
+    Hello, Next.js world!
+    <svg id="some-graphics" data-name="some-graphics" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300">
+      <text x={isSMILAvailable() ? "0" : "100"} y="20" fontFamily="sans-serif" fontSize="16" fill="#f4267c">
+        SVG Text
+        {isSMILAvailable() ? "Ok" : "No"}
+      </text>
+    </svg>
+  </div>
